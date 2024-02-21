11a69a464eacf6b2b32a6c16a339334672ae77f9
kaushik raj <algorithmunloack@gmail.com>
2024-02-21 12:45:44 +0530

    The vector is stilll going with the memeory allocation at the lowset level

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,18 @@
-
+use std::alloc::{alloc,Layout};
 
 fn main(){
+    let layout = Layout::array::<u32>(16).expect("overflow cannnot happen");
+    let vec = unsafe{
+        let mem = alloc (layout).cast::<u32>();
+        if mem.is_null(){
 
-
-
-}
+            return;
+        }
+        mem.write(4294967295);
+        Vec::from_raw_parts(mem, 1, 16);
+     
+    };
+    /*assert_eq!(vec,&[1_000_000]);
+    assert_eq!(vec.capacity(),16); */
+ //println!("The value is {:?}",layout)
+ }
