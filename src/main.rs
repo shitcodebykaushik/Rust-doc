8317a3246f7c3271664ede6f131034d5908ea45a
kaushik raj <algorithmunloack@gmail.com>
2024-03-01 16:30:21 +0530

    Stack dependices has benn solved

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,21 @@
 
-use std::io;
+#[warn(unsafe_code,unused_allocation,unused_imports)]
+use stack_stack::{Stack, stack};
 fn main (){
-    println!("Enter your name ");
-    let mut l = String::new();
-    io::stdin().read_line(&mut l ).err();   
-    println!("Your name is {}",l);
+    let mut s = Stack::with_capacity::<3>();
+ 
+    assert_eq!(s.push(6), Ok(()));
+    assert_eq!(s.push(2), Ok(()));
+    assert_eq!(s.push(8), Ok(()));
+    assert_eq!(s, [6, 2, 8]);
+     
+    assert_eq!(s.push(3), Err(3));
+    assert_eq!(s.push(1), Err(1));
+    assert_eq!(s, [6, 2, 8]);
+   
+   println!("the  {:?}",s);
+   
+    
 
-}
+    
+} 
