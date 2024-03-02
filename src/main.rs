c4ba5cfed2f0a1f366311090c05df4e26c5a614a
kaushik raj <algorithmunloack@gmail.com>
2024-03-02 19:02:51 +0530

    THis section sees

--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,10 @@
 
-#[warn(unsafe_code,unused_allocation,unused_imports)]
 use stack_stack::{Stack, stack};
 fn main (){
-    let mut s = Stack::with_capacity::<3>();
- 
-    assert_eq!(s.push(6), Ok(()));
-    assert_eq!(s.push(2), Ok(()));
-    assert_eq!(s.push(8), Ok(()));
-    assert_eq!(s, [6, 2, 8]);
-     
-    assert_eq!(s.push(3), Err(3));
-    assert_eq!(s.push(1), Err(1));
-    assert_eq!(s, [6, 2, 8]);
-   
-   println!("the  {:?}",s);
-   
-    
+    let mut d1: Stack<i64, 20> =Stack::with_capacity::<20>();
+   d1.push(5);
 
+   
     
-} 
+    println!("{:?}",d1);
+}
