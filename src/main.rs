61988704ae2813c7409dcfe5cd58bf512b6a02e5
kaushik raj <algorithmunloack@gmail.com>
2024-03-04 12:27:20 +0530

    The queue has been completed

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,3 @@
-
-use stack_stack::{Stack, stack};
-fn main (){
-    let mut d1: Stack<i64, 20> =Stack::with_capacity::<20>();
-   d1.push(5);
-
-   
+fn main () {
     
-    println!("{:?}",d1);
 }
