ce4636aee57db6d3cdfdc187e2dc352aef8de5b8
kaushik raj <algorithmunloack@gmail.com>
2024-03-13 11:54:50 +0530

    The tress have many error in documentation

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,10 @@
-
-#![allow(unused)]
-fn main() {
-use trees::Tree;
-let mut tree = Tree::new(9);
-println!("THE value is {}",tree);
+fn main (){
+    use trees::Tree;
+    let mut tree = Tree::new(9);
+    let  root = tree.root();
+assert!( root.has_no_child() );
+    assert_eq!( root.data (),&9);
+    let mut root = tree.root_mut();
+    *root.data_mut()=0;
+    println!("The {}",root);
 }
