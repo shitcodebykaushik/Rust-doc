1d2c78ae0ffc5c7fe5c2ed3d7056b360ce1c626a
kaushik raj <algorithmunloack@gmail.com>
2024-03-13 14:01:01 +0530

    The hashing theory has been updated

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,3 @@
 fn main (){
-    use trees::Tree;
-    let mut tree = Tree::new(9);
-    let  root = tree.root();
-assert!( root.has_no_child() );
-    assert_eq!( root.data (),&9);
-    let mut root = tree.root_mut();
-    *root.data_mut()=0;
-    println!("The {}",root);
+    
 }
