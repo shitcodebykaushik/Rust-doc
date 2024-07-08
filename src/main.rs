71abd3ac3433f927f9cb3187082fa1c58e6f9bf0
Kaushik raj <algorithmunloack@gmail.com>
2024-07-08 18:51:45 +0530

    The clean architutre is update

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,3 @@
-use std::thread;
-
-fn main() {
-    // Spawn two threads to run tasks in parallel
-    let thread1 = thread::spawn(|| {
-        println!("Task 1 running in parallel");
-    });
-
-    let thread2 = thread::spawn(|| {
-        println!("Task 2 running in parallel");
-    });
+fn main () {
     
-    // Wait for threads to complete (if necessary)
-    thread1.join().unwrap();
-    thread2.join().unwrap();
 }
