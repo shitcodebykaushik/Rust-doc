497e6ccbd71d11b6bbfad999105edb462a65fc12
Kaushik raj <algorithmunloack@gmail.com>
2024-07-12 00:13:12 +0530

    The revison is done here

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,6 @@
-fn main () {
-    
+fn main ( ) {
+let sus:i32 = 34; // This data is in the stack let connvert this into the heap 
+println! ("{:?}",sus);
+let sus: Box<i32>= Box::new(34); // This is in the heap location where it store the data into the heap location and box is smart pinter here .
+println!("{:?}",sus);
 }
