3909f84e8c65da611a76516abe438a56769a3db1
Kaushik raj <algorithmunloack@gmail.com>
2024-07-30 14:54:39 +0530

    The addition of mod file is done

---
+++ src/book.rs
@@ -0,0 +1,9 @@
+
+
+pub fn add_book(title: &str) {
+    println!("Book '{}' added to the library.", title);
+}
+
+pub fn remove_book(title: &str) {
+    println!("Book '{}' removed from the library.", title);
+}
