bdfe5d9350a61d9169e0c6599d68ca4cd504f41c
Kaushik raj <algorithmunloack@gmail.com>
2024-07-30 10:38:16 +0530

    The three comparison wad added here

--- R_1/src/main.rs
+++ R_1/src/main.rs
@@ -1,3 +1,15 @@
+
 fn main() {
-    println!("Hello, world!");
-}
+    println!("Selcting the larget number ");
+    // Create a function to pick up the larget number among three 
+    let a = 34;
+    let b= 350;
+    let c= 56;
+   if a>b && a>c{
+    println! ("The a is the greatest " )
+   }else if b>a && b>c {
+    println! ("The b is the greatest " )
+   }else  {
+    println!("C is the larget number ")
+   }
+   }
