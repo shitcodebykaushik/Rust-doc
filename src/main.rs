33474e5c156c835ba9deddaafedb65ccea95ab2c
Kaushik raj <algorithmunloack@gmail.com>
2024-07-30 14:55:34 +0530

    The mod file was tested here

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,10 @@
-fn main ()  {
-    loop{
-        let x = Box::new(5);
-        if *x == 5 {
-            let y = 45;
-            let z = 10;
-            let x = 45;
-            let t = y+z+x;
-            if t==100 {
-            println!("The value is ok {t}")
-        }
-        println!("All test case has been passed ");
-            }
-    }
+// main.rs
+
+mod book; // Declares the books module
+
+
+fn main() {
+    // Using functions from the modules
+    book::add_book("The Rust Programming Language");
+     // users::add_user("Alice");
 }
