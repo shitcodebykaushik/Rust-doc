01ac3f71af2ccabde6fbd3fb5d9a1b6684b2ac17
Kaushik raj <algorithmunloack@gmail.com>
2024-08-03 19:50:51 +0530

    The rust file was seen with the cargo build

--- src/main.rs
+++ src/main.rs
@@ -2,5 +2,8 @@ use std::io;
 fn main () {
     let mut name = String::new();
     io::stdin().read_line(&mut name ).expect("Failed to read line ");
-
+    let mut a = 56;
+    println!("THe value is {:?}",a);
+    a= 45;
+    println!("THe value is {:?}",a);
 }
