50e309ce194e83e7dded361bc534fc85f2b85aca
Kaushik raj <algorithmunloack@gmail.com>
2024-08-09 11:56:01 +0530

    The user_validation is mini project which will help the community to build strong foundation

---
+++ User_validation/target/debug/User_validation.d
@@ -0,0 +1,1 @@
+C:\Users\kaushikraj\Desktop\Rust-doc\User_validation\target\debug\User_validation.exe: C:\Users\kaushikraj\Desktop\Rust-doc\User_validation\src\main.rs
---
+++ User_validation/target/debug/deps/User_validation-21712fa5706b2f90.d
@@ -0,0 +1,5 @@
+C:\Users\kaushikraj\Desktop\Rust-doc\User_validation\target\debug\deps\User_validation-21712fa5706b2f90.exe: src/main.rs
+
+C:\Users\kaushikraj\Desktop\Rust-doc\User_validation\target\debug\deps\User_validation-21712fa5706b2f90.d: src/main.rs
+
+src/main.rs:
