17c9174540491d7b6c7b6a7a71e3f4018c12d101
kaushik <algorithmunloack@gmail.com>
2024-12-13 02:06:32 +0530

    The src patches was updated here

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,3 @@
-use std::io;
 fn main () {
-    let mut name = String::new();
-    io::stdin().read_line(&mut name ).expect("Failed to read line ");
-    let mut a = 56;
-    println!("THe value is {:?}",a);
-    a= 45;
-    println!("THe value is {:?}",a);
-    println!("Kaushik be ready for the things");
-    println! ("This is the hackthon things");
-    println!("On this day UI for the hackthon was done ");
+    println!("This is ok")
 }
